fc1e9d0314cc776f95275d1f003f85cc8b3851e7
Ilian Genov <ilian.genov@unwe.bg>
2022-01-11 10:13:14 +0200

    Initital master config commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=address-book',
+    'dsn' => 'mysql:host=localhost;dbname=address_book',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
